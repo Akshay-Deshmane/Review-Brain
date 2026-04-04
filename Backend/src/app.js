5bc8d962cad018cdad492574363e0c741d942386
Akshay-Deshmane <akshaydeshmane1087@gmail.com>
2026-04-04 17:22:20 +0530

    gemini-setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
 const app = express();
+const aiRoutes = require("./routes/ai.routes")
+
+
+app.use("/ai", aiRoutes);
+
 
 
 module.exports = app;
---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,21 @@
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({
+    apiKey : process.env.GEMINI_API_KEY
+});
+
+async function AICodeReview(prompt) {
+
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: prompt,
+  });
+
+  return response.text;
+
+}
+
+
+
+
+module.exports = AICodeReview;
